54c4fbae7892f1aaf80b6055709e6410dd69d81a
Oleg Taranenko MP <olegtaranenko@gmail.com>
2020-10-02 10:54:51 +0200

    minor fix to pass github pipeline checking

--- proto-app/src/shared/checkValidity.js
+++ proto-app/src/shared/checkValidity.js
@@ -3,7 +3,7 @@ const checkValidity = (value) => {
 
 	isValid = value.trim() !== "" && isValid;
 
-	const pattern = /^\d{8}-\d{4}\-\d{4}\-\d{4}\-\d{11}$/;
+	const pattern = /^\d{8}-\d{4}-\d{4}-\d{4}-\d{11}$/;
 	isValid = pattern.test(value.replace(/\s+/g, "")) && isValid;
 	return isValid;
 };
