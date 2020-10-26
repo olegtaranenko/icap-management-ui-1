6598dea7fdb782f9ed99828ec43b3fe740675108
werzl <adam@houseofhewitt.co.uk>
2020-10-26 12:44:23 +0000

    Update Config.ts

--- server/src/service/Config.ts
+++ server/src/service/Config.ts
@@ -2,8 +2,8 @@ import IConfig from "../common/models/IConfig";
 
 const Config = () => {
     const development: IConfig = {
-        transactionEventServiceBaseUrl: process.env.TRANSACTION_EVENT_SERVICE_BASE_URL,
-        getTransactionsPath: process.env.TRANSACTION_EVENT_SERVICE_GET_TRANSACTIONS_PATH
+        transactionEventServiceBaseUrl: "https://transactioneventapifunction20201016103513.azurewebsites.net/api/v1",
+        getTransactionsPath: "/transactions"
     };
 
     const production: IConfig = {
