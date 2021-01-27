e98b15b1f2d750491ac8e81e84edc06d18b394d9
werzl <adam@houseofhewitt.co.uk>
2021-01-27 01:57:50 +0000

    Update getTranasctions.ts

--- server/frontend/src/views/RequestHistory/api/getTranasctions.ts
+++ server/frontend/src/views/RequestHistory/api/getTranasctions.ts
@@ -6,27 +6,19 @@ import { GetTransactionsResponse } from "../../../../../src/common/models/Transa
 const requestHistoryRoutes = Routes.requestHistoryRoutes;
 
 export const getTransactions = async (body: Filter, cancellationToken: CancelToken): Promise<GetTransactionsResponse> => {
-    try {
+    const response = await axios(requestHistoryRoutes.getTransactionsRoute, {
+        method: "POST",
+        data: JSON.stringify({ Filter: body }),
+        headers: {
+            'Accept': '*/*',
+            'Content-Type': 'application/json'
+        },
+        cancelToken: cancellationToken
+    });
 
-        const response = await axios(requestHistoryRoutes.getTransactionsRoute, {
-            method: "POST",
-            data: JSON.stringify({ Filter: body }),
-            headers: {
-                'Accept': '*/*',
-                'Content-Type': 'application/json'
-            },
-            cancelToken: cancellationToken
-        });
-
-        if (response.status < 200 || response.status > 299) {
-            throw new Error(response.data);
-        }
-
-        return response.data;
+    if (response.status < 200 || response.status > 299) {
+        throw response.data;
     }
-    catch (error) {
-        // tslint:disable-next-line: no-console
-        console.error("getTransactions: " + error);
-        throw error;
-    }
+
+    return response.data;
 };
